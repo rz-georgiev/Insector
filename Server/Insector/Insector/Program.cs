401247ea72fac265541ee99b687c3352e6d0a2ec
Rado. G <rzgeorgiev@outlook.com>
2022-08-09 16:32:34 +0300

    Rearranging program entry point

--- Server/Insector/Insector/Program.cs
+++ Server/Insector/Insector/Program.cs
@@ -3,7 +3,6 @@ using Insector.Data.Interfaces;
 using Insector.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -15,6 +14,7 @@ namespace Main
     internal class Program
     {
         private static IConfiguration _configuration;
+        private static IServiceCollection _services;
 
         private static void Main(string[] args)
         {
@@ -23,24 +23,48 @@ namespace Main
 
             _configuration = configurationBuilder.Build();
 
-
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers(options =>
+            _services = builder.Services;
+
+            ConfigureIdentity();
+            ConfigureJwt();
+            ConfigureServices();
+            ConfigureDbContext();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.MapControllers();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.Run();
+        }
+
+        private static void ConfigureIdentity()
+        {
+            _services.AddControllers(options =>
             {
                 var policy = new AuthorizationPolicyBuilder()
                                .RequireAuthenticatedUser()
                                .Build();
                 options.Filters.Add(new AuthorizeFilter(policy));
             });
-            builder.Services.AddEndpointsApiExplorer();
 
-            builder.Services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo
+            _services.AddEndpointsApiExplorer();
+            _services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "JWTToken_Auth_API",
                     Version = "v1"
                 });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
                     Name = "Authorization",
                     Type = SecuritySchemeType.ApiKey,
@@ -49,21 +73,27 @@ namespace Main
                     In = ParameterLocation.Header,
                     Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
                 });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                }
-            },
-                        new string[] {}
+
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
                     }
                 });
             });
+        }
 
-            builder.Services.AddMvc(); // ?
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        private static void ConfigureJwt()
+        {
+            _services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
                     options.TokenValidationParameters = new TokenValidationParameters
@@ -74,41 +104,22 @@ namespace Main
                         ValidateIssuerSigningKey = true,
                         ValidIssuer = _configuration["Jwt:Issuer"],
                         ValidAudience = _configuration["Jwt:Audience"],
-                        IssuerSigningKey =
-                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]))
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]))
                     };
                 });
-
-
-            ConfigureServices(builder.Services);
-            ConfigureDbContext(builder.Services);
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();          
-            app.MapControllers();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.Run();
         }
 
-        private static void ConfigureServices(IServiceCollection services)
+        private static void ConfigureServices()
         {
-            services.AddTransient<IUserService, UserService>();
+            _services.AddTransient<IUserService, UserService>();
         }
 
-        private static void ConfigureDbContext(IServiceCollection services)
+        private static void ConfigureDbContext()
         {
             var connectionString = _configuration.GetConnectionString("Insector");
             var serverVersion = new MySqlServerVersion(MySqlServerVersion.LatestSupportedServerVersion);
 
-            services.AddDbContext<InsectorDbContext>(
+            _services.AddDbContext<InsectorDbContext>(
                 dbContextOptions => dbContextOptions
                     .UseMySql(connectionString, serverVersion)
                     .LogTo(Console.WriteLine, LogLevel.Information)
